3302929559d039e7144c61df877edeee6cf54f83
cameronalbin <cameron.tex.albin@gmail.com>
2020-04-10 15:34:39 -0500

    v9

--- quotestool/models.py
+++ quotestool/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Parameter(models.Model):
+    name = models.CharField(max_length=200)
+    quantity = models.IntegerField(default=0)
+    def getQuantity(self):
+        return self.quantity
+    def __str__(self):
+        return self.name + " = " + str(self.quantity)
