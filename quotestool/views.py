abe51a3d90fbd35cb2de72ecf13a00087d3572e1
cameronalbin <cameron.tex.albin@gmail.com>
2020-04-10 15:44:44 -0500

    v11

--- quotestool/views.py
+++ quotestool/views.py
@@ -1,8 +1,29 @@
 from django.http import HttpResponse
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
 
+from .models import Parameter
+#from .prediction import predict
 
 def input(request):
-    return HttpResponse("This is the input page")
+    parameterList = Parameter.objects.all()
+    context = {'parameterList': parameterList}
+    return HttpResponse('input page')
+#    return render(request, 'quotesTool/input.html', context)
 
 def results(request):
-    return HttpResponse("This is the results page")
+    parameterList = Parameter.objects.all()
+#    smc = Parameter.objects.get(name="SMC")
+#    eau = Parameter.objects.get(name="EAU")
+#    smc.quantity = int(request.GET['SMC'])
+#    eau.quantity = int(request.GET['EAU'])
+#    password = request.GET['pwd']
+#    smc.save()
+#    eau.save()
+#    if password == 'alphaplan':
+#        prediction = predict(smc.quantity,eau.quantity)
+#        context = {'parameterList': parameterList, 'prediction':prediction}
+#        return render(request, 'quotesTool/results.html', context)
+#    return HttpResponse('wrong password')
+    return HttpResponse('results page')
