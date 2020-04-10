143a6422775a3a9593108b9e33c1d7e02d68c14a
cameronalbin <cameron.tex.albin@gmail.com>
2020-04-10 16:31:35 -0500

    v17

--- quotestool/views.py
+++ quotestool/views.py
@@ -6,15 +6,20 @@ from django.shortcuts import render
 from .models import Parameter
 #from .prediction import predict
 
+Parameter.objects.all().delete()
+smc = Parameter(name="SMC")
+eau = Parameter(name="EAU")
+smc.save()
+eau.save()
+
 def input(request):
     parameterList = Parameter.objects.all()
     context = {'parameterList': parameterList}
-#    return HttpResponse('input page')
     return render(request, 'quotestool/input.html', context)
 
 def results(request):
     parameterList = Parameter.objects.all()
-    smc = Parameter.objects.get(name="SMC")
+#    smc = Parameter.objects.get(name="SMC")
 #    eau = Parameter.objects.get(name="EAU")
 #    smc.quantity = int(request.GET['SMC'])
 #    eau.quantity = int(request.GET['EAU'])
